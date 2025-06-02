8824b82835602771c39303115a0a54f5f6012515
Ugalino_Fredierick_BSIT2E <ufredierick@gmail.com>
2025-06-02 23:55:44 +0800

    Delete Ayokona directory

--- Ayokona/dashboard.js
+++
@@ -1,5 +0,0 @@
-function redirectToBooking(title, genre, duration, summary, image) {
-  // Encode the movie details and pass them as query parameters to booking.php
-  const bookingUrl = `booking.html?title=${encodeURIComponent(title)}&genre=${encodeURIComponent(genre)}&duration=${encodeURIComponent(duration)}&summary=${encodeURIComponent(summary)}&image=${encodeURIComponent(image)}`;
-  window.location.href = bookingUrl; // Redirect to the booking page with the parameters
-}
